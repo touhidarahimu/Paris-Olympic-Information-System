93ae817cc25c0e6069bcb787034a63f20c29576d
Touhid Ara Himu <touhidarahimu30@gmail.com>
2024-12-12 12:25:23 +0600

    Write main function

--- code/Athletics.h
+++ code/Athletics.h
@@ -19,14 +19,56 @@ struct athletics
 extern athletics *head;
 extern athletics *tail;
 
-athletics *createAthletics(int code_number);
-athletics *createAthletics(string name);
-athletics *createAthletics(athletics *ath);
-athletics *createAthletics(int code_number,string name);
+athletics *createAthletics(int code_number);//done
+athletics *createAthletics(string name);//done
+athletics *createAthletics(athletics *ath);//done
+athletics *createAthletics(int code_number,string name);//done
 
-void insertAthletics(int code_number);
-void insertAthletics(athletics *ath);
-void insertAthletics(int code_number,string name);
+void insertAthletics(int code_number);//done
+void insertAthletics(athletics *ath);//done
+void insertAthletics(int code_number,string name);//done
+
+void updateData();//done
+void updateInfoCodeNumber();//done
+
+ void deleteNode(athletics *delNode);
+void deleteAllInfo();//done
+void deleteCodeNumber(int code_number);//done
+void deleteName(string name);//done
+
+bool foundCodeNumber(int code_number);//done
+bool foundName(string name);//done
+bool isComplete(athletics *ath);//done
+bool compareString(string s1, string s2);//done
+
+
+void searchCodeNumber(int code_number);
+void searchName(string name);
+void searchGender(string gender);
+void searchCountryCode(string country_code);
+void searchCountry(string country);
+void searchLanguage(string language);
+
+
+void sortCodeNumber( int code_number);
+void sortName(string name);
+void sortCountryCode(string country_code);
+void sortCountry(string country);
+void sortLanguage(string language);
+
+
+int menu();
+void showAthleticsInfo();
+void addAthleticsInfo();
+void updateAthleticsInfo();
+void deleteAthleticsInfo();
+void searchAthleticsInfo();
+void sortAthleticsInfo();
+
+
+void inputManipulation();
+void outputManipulation();
+
 
 
 
--- code/Athletics_main.cpp
+++ code/Athletics_main.cpp
@@ -4,27 +4,37 @@
 using namespace std;
 
 int main() {
-    // Create athletics objects using different constructors
-    athletics *ath1 = createAthletics(1003);              // Create using code number
-    athletics *ath2 = createAthletics("mark ");       // Create using name
-    athletics *ath3 = createAthletics(ath1);            // Copy an existing athletics object
-    athletics *ath4 = createAthletics(1002, "Jane Doe"); // Create using code number and name
-
-    // Display details of the athletics objects
-    cout << "Athlete 1: Code = " << ath1->code_number << ", Name = " << ath1->name << endl;
-    cout << "Athlete 2: Code = " << ath2->code_number << ", Name = " << ath2->name << endl;
-    cout << "Athlete 3 (Copy of Athlete 1): Code = " << ath3->code_number << ", Name = " << ath3->name << endl;
-    cout << "Athlete 4: Code = " << ath4->code_number << ", Name = " << ath4->name << endl;
-
-    // Free dynamically allocated memory
-    delete ath1;
-    delete ath2;
-    delete ath3;
-    delete ath4;
-
-    return 0;
+    inputManipulation();
+    int input;
+    while (input != 0) {
+        input = menu();
+        if (input == 1) {
+            showAthleticsInfo();
+            cout<<"\nPress any key to return Main Menu...";
+            getchar();
+        } else if (input == 2) {
+            addAthleticsInfo();
+        } else if (input == 3) {
+            updateAthleticsInfo();
+        } else if (input == 4) {
+            deleteAthleticsInfo();
+        } else if (input == 5) {
+            searchAthleticsInfo();
+        } else if (input == 6) {
+            sortAthleticsInfo();
+        } 
+        else if (input == 0) {
+            outputManipulation();
+            exit(0);
+        } else {
+            cout << "\n\t\t\tInvalid Option!!" << "\n";
+            cout << "\t\t       Please try again..." << "\n";
+            cout << "\n";
+            cout << "Press any key to continue...";
+            getchar();
+        }
+    }
 }
-
 
 //zsh terminal run command
 // cd "/Users/himu/Desktop/Paris Olympic Information System/code/"
